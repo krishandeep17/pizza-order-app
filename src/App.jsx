50ca24b5a4d1948c4b731a4dd4804afc3657ebc9
Krishandeep Singh <krishandeep17@gmail.com>
2023-10-03 12:17:09 +0530

    implement routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Cart from "./features/cart/Cart";
+import Menu from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
+]);
+
 export default function App() {
-  return (
-    <div>
-      <h1>Pizza Order App</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <Link to="/">Pizza Order App</Link>
+
+      <p>Krishandeep</p>
+    </header>
+  );
+}
