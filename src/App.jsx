61208fcb71d3fd3a873423efe23e7d80d1b69bcc
Krishandeep Singh <krishandeep17@gmail.com>
2023-10-03 18:20:58 +0530

    handle errors with errorElement

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import Menu, { menuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 import Home from "./ui/Home";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    // When a route does not have an `errorElement`, errors will bubble up through parent routes.
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
       {
         path: "/menu",
         element: <Menu />,
         loader: menuLoader, // `loader` function provide data to the route element before it renders
+        errorElement: <Error />, // it renders when exceptions are thrown in loaders, actions, or component rendering
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,12 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default function Error() {
   const navigate = useNavigate();
 
+  // this hook returns anything thrown during an action, loader, or rendering
+  const error = useRouteError();
+
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <p>{error.data || error.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
